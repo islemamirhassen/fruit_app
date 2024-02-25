14b20a025f59674cf7e0e9348d7982ee1bcdab45
= <=>
2024-02-25 10:25:09 +0100

    fix router module error

--- src/app/fruits/home/home.component.ts
+++ src/app/fruits/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Fruits } from '../fruits';
 import { FruitsService } from '../fruits.service';
 import { HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   providers: [HttpClient],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
